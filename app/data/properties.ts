97adbf244d4d42617bf078fa5ec3b374004dacc0
faran-studio <farankarimi.1970@gmail.com>
2026-08-08 18:51:57 +0330

    move property data into data file

---
+++ app/data/properties.ts
@@ -0,0 +1,50 @@
+export const properties = [
+  {
+    title: "ویلا ساحلی",
+    location: "نوشهر",
+    description: "ویوی دریا",
+    price: "۲۵ میلیارد تومان",
+    image: "/images/apartment1.jpg",
+  },
+
+  {
+    title: "ویلا جنگلی",
+    location: "نوشهر",
+    description: "چشم‌انداز جنگل",
+    price: "۲۸ میلیارد تومان",
+    image: "/images/land1.jpg",
+  },
+
+  {
+    title: "آپارتمان لوکس",
+    location: "رشت",
+    description: "دسترسی عالی",
+    price: "۱۲ میلیارد تومان",
+    image: "/images/villa1.jpg",
+  },
+
+  {
+    title: "آپارتمان نوساز",
+    location: "رشت",
+    description: "نورگیر و مدرن",
+    price: "۱۰ میلیارد تومان",
+    image: "/images/apartment2.jpg",
+  },
+
+  {
+    title: "زمین سرمایه‌گذاری",
+    location: "نوشهر",
+    description: "مناسب ساخت ویلا",
+    price: "۸ میلیارد تومان",
+    image: "/images/land2.jpg",
+  },
+
+  {
+    title: "زمین مسکونی",
+    location: "رشت",
+    description: "موقعیت عالی",
+    price: "۷ میلیارد تومان",
+    image: "/images/Villa2.jpg",
+
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -2,56 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Hero from "@/app/components/hero/Hero";
 import Navigation from "@/app/components/navigation/Navigation";
-const properties = [
-  {
-    title: "ویلا ساحلی",
-    location: "نوشهر",
-    description: "ویوی دریا",
-    price: "۲۵ میلیارد تومان",
-    image: "/images/apartment1.jpg",
-  },
-
-  {
-    title: "ویلا جنگلی",
-    location: "نوشهر",
-    description: "چشم‌انداز جنگل",
-    price: "۲۸ میلیارد تومان",
-    image: "/images/land1.jpg",
-  },
-
-  {
-    title: "آپارتمان لوکس",
-    location: "رشت",
-    description: "دسترسی عالی",
-    price: "۱۲ میلیارد تومان",
-    image: "/images/villa1.jpg",
-  },
-
-  {
-    title: "آپارتمان نوساز",
-    location: "رشت",
-    description: "نورگیر و مدرن",
-    price: "۱۰ میلیارد تومان",
-    image: "/images/apartment2.jpg",
-  },
-
-  {
-    title: "زمین سرمایه‌گذاری",
-    location: "نوشهر",
-    description: "مناسب ساخت ویلا",
-    price: "۸ میلیارد تومان",
-    image: "/images/land2.jpg",
-  },
-
-  {
-    title: "زمین مسکونی",
-    location: "رشت",
-    description: "موقعیت عالی",
-    price: "۷ میلیارد تومان",
-    image: "/images/Villa2.jpg",
-
-  },
-];
+import { properties } from "@/app/data/properties";
 
 export default function Home() {
   return (
